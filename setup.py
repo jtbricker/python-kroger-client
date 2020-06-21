03cdbaf2c4e460320826f9dfbcb2c83eee6891cf
jtbricker <jt.bricker@gmail.com>
2020-06-21 18:30:23 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='python_kroger_client',
+      version='0.1',
+      description="Wrapper around Kroger's api",
+      url='http://github.com/jtbricker/python-kroger-client',
+      author='Justin Bricker',
+      author_email='jt.bricker@gmail.com',
+      license='MIT',
+      packages=['python_kroger_client'],
+      zip_safe=False)
